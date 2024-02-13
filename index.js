f17b6005e0f06465978596cfcadb9e606b8f24a3
koinoniays2 <koinonia_ys@naver.com>
2024-02-13 22:09:49 +0900

    포트폴리오 수정

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-$(function(){
+$(document).ready(function() {
+    $(".fa-solid").click(function(){
+        $("#menu").fadeToggle();
+    });
+
     $('nav a').click(function(e){
         $.scrollTo(this.hash || 0, 400); // 속도 조절, 숫자가 작을수록 빠름
         e.preventDefault();
-    })
-})
+    });
+});
