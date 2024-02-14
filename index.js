514a3f63506e07223727c6d9ae82603ebefd966b
koinoniays2 <koinonia_ys@naver.com>
2024-02-15 03:51:04 +0900

    헤더 백그라운드 수정

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ $(document).ready(function () {
 // 데스크톱 브라우저인 경우 마우스 호버를 통해 서브메뉴를 표시, 호버시 배경색 변경
     if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         $('#menu-nav ul li').mouseenter(function () {
-            $(this).find('.sub-menu').stop().slideDown(200);
+            $(this).find('.sub-menu').stop().slideDown(300);
+            $('#header').css('background-color', 'white');
         }).mouseleave(function () {
-            $(this).find('.sub-menu').stop().slideUp(200);
+            $(this).find('.sub-menu').stop().slideUp(300);
+            $('#header').css('background-color', '');
         });
         
         $('#header .sub-menu li').hover(function() {
@@ -28,34 +30,38 @@ $(document).ready(function () {
             let submenu = $(this).find('.sub-menu');
             // 현재 서브메뉴가 열려있는지 확인하여 토글
             if (submenu.is(':visible')) {
-                submenu.stop().slideUp(200);
+                submenu.stop().slideUp(300);
             } else {
                 // 다른 서브메뉴를 닫은 후 현재 서브메뉴 열기
-                $('.sub-menu').not(submenu).slideUp(200);
-                submenu.stop().slideDown(200);
+                $('.sub-menu').not(submenu).slideUp(300);
+                submenu.stop().slideDown(300);
             }
         });
     }
 
 // 스크롤이 발생하면 메뉴 닫기
-    var isToggled = false;
+    let scrollTimeout;
     $(window).scroll(function() {
+// 스크롤이 멈추면 1.5초 후 배경색 돌아옴
+        clearTimeout(scrollTimeout);
+        scrollTimeout = setTimeout(function() {
+            const windowWidth = $(window).width();
+            if (windowWidth <= 640) {
+                $('#menu-nav').slideUp(300);
+            }
+            $('#header').css('background-color', '');
+        }, 150);
+// 스크롤 중일 때 즉시 배경색 변경
         $('.sub-menu').slideUp(300);
-        const windowWidth = $(window).width();
-        if (windowWidth <= 640) {
-            $('#menu-nav').slideUp(300);
-        }
-        $('#header').css('background-color', '');
-        isToggled = !isToggled;
+        $('#header').css('background-color', 'white');
     });
 // 토글 클릭시 헤더 백그라운드
     $('#toggle-icon').click(function() {
-        if (isToggled) {
+        const headerBackgroundColor = $('#header').css('background-color');
+        if (headerBackgroundColor === 'rgb(255, 255, 255)' || headerBackgroundColor === '#ffffff') {
             $('#header').css('background-color', '');
         } else {
             $('#header').css('background-color', '#ffffff');
         }
-        isToggled = !isToggled;
     });
-
 });
